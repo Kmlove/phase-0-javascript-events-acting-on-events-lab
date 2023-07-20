ceb048d992f4c771f58ac79240ee305e08017faf
Kim Love <kimberlymlove15@gmail.com>
2023-07-19 22:24:56 -0400

    Allows the dodger to move left and right. All tests are passsing.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  } else{
+    alert("You're at the left edge of the page! Move right!");
+  }
+}
+
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  } else{
+    alert("You're at the right edge of the page! Move left!");
+  }
+}
+
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
